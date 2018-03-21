8cd7839ff12622d3d28cf5ac064929569b17f27c
Eleanore Young <youngelliecaitlin@gmail.com>
2018-03-21 21:01:02 +0100

    Added the main binary.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+#[macro_use] extern crate log;
+extern crate fern;
+extern crate game;
+
+use std::env;
+use std::io;
+use std::time::Duration;
+use log::LevelFilter;
+use fern::Dispatch;
+use game::Game;
+
 fn main() {
-    println!("Hello, world!");
+    Dispatch::new()
+        .format(|out, message, record| {
+            out.finish(format_args!(
+                "{} @{}: {}",
+                record.level(),
+                record.target(),
+                message
+            ))
+        })
+        .level(LevelFilter::Debug)
+        .chain(io::stdout())
+        .apply()
+        .unwrap_or_else(|e| error!("Error setting up the logger: {}", e));
+
+    match Game::new(&env::temp_dir(), Duration::from_millis(50), Duration::from_millis(250)) {
+        Ok(mut game) => if let Err(e) = game.run(None) {
+            error!("The game aborted with a runtime error: {}", e)
+        },
+        Err(e) => error!("Creation of the game failed with: {}", e),
+    }
 }
