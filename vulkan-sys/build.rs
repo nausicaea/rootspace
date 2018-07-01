32ee9fc28279dfdf5f78e726dd689777ba6a5672
Eleanore Young <youngelliecaitlin@gmail.com>
2018-07-01 16:02:58 +0200

    Added the vulkan headers to the crate to allow binding generation on multiple different platforms.

--- vulkan-sys/build.rs
+++ vulkan-sys/build.rs
@@ -4,13 +4,29 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
-    // Tell cargo to link this library against
-    println!("cargo:rustc-link-lib=vulkan");
-    println!("cargo:rustc-link-search={}/lib", env::var("VULKAN_SDK").unwrap());
+    let macos_err: &'static str = "Unable to find the Vulkan SDK, which is required on MacOS";
 
-    let bindings = bindgen::Builder::default()
-        .header("wrapper.h")
-        .clang_arg(format!("-I{}/include", env::var("VULKAN_SDK").unwrap()))
+    // Adjust the library search path for MacOs.
+    #[cfg(target_os = "macos")]
+    println!("cargo:rustc-link-search={}/lib", env::var("VULKAN_SDK").expect(macos_err));
+
+    // Tell rustc to link against the vulkan library.
+    println!("cargo:rustc-link-lib=vulkan");
+
+    // Generate the rust bindings for vulkan.
+    let builder = bindgen::Builder::default()
+        .header("vulkan/vulkan.h");
+
+    #[cfg(target_os = "ios")]
+    let builder = builder.clang_arg("-DVK_USE_PLATFORM_IOS_MVK");
+
+    #[cfg(target_os = "macos")]
+    let builder = builder.clang_arg("-DVK_USE_PLATFORM_MACOS_MVK");
+
+    #[cfg(target_os = "windows")]
+    let builder = builder.clang_arg("-DVK_USE_PLATFORM_WIN32_KHR");
+
+    let bindings = builder
         .generate()
         .expect("Unable to generate the bindings");
 
