fe1d2ef034df7ffa6671d02a9a504f0590a34372
Eleanore Young <youngelliecaitlin@gmail.com>
2018-08-12 19:48:44 +0200

    Continued working on the PLY parser.

--- ply/tests/ply_ascii.rs
+++ ply/tests/ply_ascii.rs
@@ -1,8 +1,11 @@
 #[macro_use]
 extern crate assertions;
 extern crate ply;
+extern crate combine;
+
+use combine::parser::Parser;
 
 #[test]
 fn header() {
-    assert_ok!(ply::header(include_bytes!("example_ascii.ply")));
+    assert_ok!(ply::header().parse(&include_bytes!("example_ascii.ply")[..]));
 }
