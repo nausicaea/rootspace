a476ba43183519e390ac048add642753e53ad6e4
Eleanore Young <youngelliecaitlin@gmail.com>
2018-10-27 22:51:05 +0200

    Used the wrong keyword for texture samplers in GLSL.

--- engine/src/graphics/glium/mod.rs
+++ engine/src/graphics/glium/mod.rs
@@ -286,8 +286,8 @@ pub fn triangle(backend: &GliumBackend) -> Result<GliumRenderData, Error> {
                 #version 330 core
 
                 uniform vec2 dimensions;
-                uniform Sampler2D diffuse_texture;
-                uniform Sampler2D normal_texture;
+                uniform sampler2D diffuse_texture;
+                // uniform sampler2D normal_texture;
 
                 out vec4 color;
 
--- resources/rootspace/shaders/text-fragment.glsl
+++ resources/rootspace/shaders/text-fragment.glsl
@@ -1,8 +1,8 @@
 #version 330 core
 
 uniform vec2 dimensions;
-uniform Sampler2D diffuse_texture;
-uniform Sampler2D normal_texture;
+uniform sampler2D diffuse_texture;
+// uniform sampler2D normal_texture;
 
 out vec4 color;
 
