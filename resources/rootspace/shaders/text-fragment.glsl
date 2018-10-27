726d3f8c3e8c05221d0279bb4ff808f3a72482a8
Eleanore Young <youngelliecaitlin@gmail.com>
2018-10-27 23:42:53 +0200

    Sigh...

--- resources/rootspace/shaders/text-fragment.glsl
+++ resources/rootspace/shaders/text-fragment.glsl
@@ -8,7 +8,7 @@ in vec2 frag_tex_coord;
 
 out vec4 color;
 
-const vec4 text_color = vec4(0.0, 0.0, 0.0, 1.0);
+const vec3 text_color = vec3(0.0, 0.0, 0.0);
 
 void main() {
     vec4 text_data = texture(diffuse_texture, frag_tex_coord);
