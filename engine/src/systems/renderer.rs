06426092c15025cdafd6a885ee15c35cc42c3c2a
Eleanore Young <youngelliecaitlin@gmail.com>
2018-10-28 14:42:32 +0100

    Corrected a small typo

--- engine/src/systems/renderer.rs
+++ engine/src/systems/renderer.rs
@@ -79,7 +79,7 @@ where
         let nodes = ctx.get_nodes(true);
 
         // Obtain a reference to the camera.
-        let cam = ctx.find::<Cam>().map_err(|e| format_err!("{} (Camera", e))?;
+        let cam = ctx.find::<Cam>().map_err(|e| format_err!("{} (Camera)", e))?;
 
         // Create a new frame.
         let mut target = self.backend.create_frame();
