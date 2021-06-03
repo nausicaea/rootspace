c31cf98fcf5c8b5b63b79b14a0741b2fc1ccf48f
Eleanore Young <developer@nausicaea.net>
2021-06-03 09:30:41 +0200

    Removed EmptyProxyError, and added a TODO

--- ecs/src/serialization_name.rs
+++ ecs/src/serialization_name.rs
@@ -1,15 +1,7 @@
 use crate::short_type_name;
 use std::fmt::{Debug, Display};
 
-#[derive(Copy, Clone, Debug)]
-pub struct EmptyProxyError;
-
-impl Display for EmptyProxyError {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        f.write_str(std::any::type_name::<Self>())
-    }
-}
-
+// TODO: Solve this differently; perhaps by accessing a property in Serialize/Deserialize?
 pub trait SerializationName: Sized {
     fn name() -> String {
         short_type_name::<Self>()
