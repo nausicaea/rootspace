47a353c6b72b8356ed7fcc3eaf7f0241c6dbce9e
Eleanor Young <developer@nausicaea.net>
2022-12-11 17:33:57 +0100

    Added tests for format_stmt, comment_stmt, and obj_info_stmt

--- plyers2/src/parser.rs
+++ plyers2/src/parser.rs
@@ -1,4 +1,4 @@
-use nom::{bytes::complete::{tag, take_while1, take_till1}, IResult, branch::alt, sequence::{pair, delimited, separated_pair}, character::{is_space, is_newline}};
+use nom::{bytes::complete::{tag, take_while1, take_till1}, IResult, branch::alt, sequence::{pair, delimited, separated_pair, terminated}, character::{is_space, is_newline}};
 
 const PLY: &'static [u8] = b"ply";
 const END_HEADER: &'static [u8] = b"end_header";
@@ -28,34 +28,12 @@ const UINT32: &'static [u8] = b"uint32";
 const FLOAT32: &'static [u8] = b"float32";
 const FLOAT64: &'static [u8] = b"float64";
 
-/// Tests if byte is ASCII space or tab
-///
-/// # Example
-///
-/// ```
-/// # use plyers2::parser::is_delimiter;
-/// assert_eq!(is_delimiter(0x09), true);
-/// assert_eq!(is_delimiter(0x0a), true);
-/// assert_eq!(is_delimiter(0x0b), true);
-/// assert_eq!(is_delimiter(0x0c), true);
-/// assert_eq!(is_delimiter(0x0d), true);
-/// assert_eq!(is_delimiter(0x20), true);
-/// ```
-#[inline]
-pub fn is_delimiter(chr: u8) -> bool {
-    (chr >= 0x09 && chr <= 0x0d) || (chr == 0x20)
-}
-
 fn space(input: &[u8]) -> IResult<&[u8], &[u8]> {
     take_while1(is_space)(input)
 }
 
 fn newline(input: &[u8]) -> IResult<&[u8], &[u8]> {
     take_while1(is_newline)(input)
-}
-
-fn delimiter(input: &[u8]) -> IResult<&[u8], &[u8]> {
-    take_while1(is_delimiter)(input)
 }
 
 fn single_line_text(input: &[u8]) -> IResult<&[u8], &[u8]> {
@@ -87,7 +65,7 @@ fn format_version(input: &[u8]) -> IResult<&[u8], &[u8]> {
 }
 
 fn format_stmt(input: &[u8]) -> IResult<&[u8], &[u8]> {
-    delimited(pair(format_kwd, space), format_type, pair(space, format_version))(input)
+    terminated(delimited(pair(format_kwd, space), format_type, pair(space, format_version)), newline)(input)
 }
 
 fn comment_kwd(input: &[u8]) -> IResult<&[u8], &[u8]> {
@@ -95,8 +73,17 @@ fn comment_kwd(input: &[u8]) -> IResult<&[u8], &[u8]> {
 }
 
 fn comment_stmt(input: &[u8]) -> IResult<&[u8], (&[u8], &[u8])> {
-    separated_pair(comment_kwd, space, single_line_text)(input)
+    terminated(separated_pair(comment_kwd, space, single_line_text), newline)(input)
 }
+
+fn obj_info_kwd(input: &[u8]) -> IResult<&[u8], &[u8]> {
+    tag(OBJ_INFO)(input)
+}
+
+fn obj_info_stmt(input: &[u8]) -> IResult<&[u8], (&[u8], &[u8])> {
+    terminated(separated_pair(obj_info_kwd, space, single_line_text), newline)(input)
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -105,25 +92,37 @@ mod tests {
 
     use super::*;
 
+    const EMPTY: &'static [u8] = b"";
+
     proptest!{
         #[test]
         fn space_matches_x09_and_x20(ref input in bytes_regex(r"[\x09\x20]+").unwrap()) {
-            prop_assert_eq!(dbg_dmp(space, "proptest_space")(&input[..]), Ok((&b""[..], &input[..])))
+            prop_assert_eq!(dbg_dmp(space, "proptest_space")(&input[..]), Ok((EMPTY, &input[..])))
         }
 
         #[test]
         fn newline_matches_x0a(ref input in bytes_regex(r"\x0a+").unwrap()) {
-            prop_assert_eq!(dbg_dmp(newline, "proptest_newline")(&input[..]), Ok((&b""[..], &input[..])))
-        }
-
-        #[test]
-        fn delimiter_matches_ascii_whitespace(ref input in bytes_regex(r"[\x09\x0a\x0b\x0c\x0d\x20]").unwrap()) {
-            prop_assert_eq!(dbg_dmp(delimiter, "proptest_delim")(&input[..]), Ok((&b""[..], &input[..])))
+            prop_assert_eq!(dbg_dmp(newline, "proptest_newline")(&input[..]), Ok((EMPTY, &input[..])))
         }
 
         #[test]
         fn single_line_text_matches_anything_till_exclusive_x0a(ref input in bytes_regex(r"[\w]+\x0a").unwrap()) {
             prop_assert_eq!(dbg_dmp(single_line_text, "proptest_single_line_text")(&input[..]), Ok((&b"\n"[..], &input[..input.len()-1])))
+        }
+
+        #[test]
+        fn format_stmt_matches_keyword_type_version_and_a_newline_returns_only_type(ref input in bytes_regex(r"format[\x09\x20](ascii|binary_big_endian|binary_little_endian)[\x09\x20]1\.0\x0a").unwrap()) {
+            assert_eq!(format_stmt(&input[..]), Ok((EMPTY, &input[7..input.len()-5])))
+        }
+
+        #[test]
+        fn comment_stmt_matches_keyword_freetext_and_a_newline_returns_everything(ref input in bytes_regex(r"comment[\x09\x20][\w]+\x0a").unwrap()) {
+            prop_assert_eq!(dbg_dmp(comment_stmt, "proptest_comment")(&input[..]), Ok((EMPTY, (COMMENT, &input[8..input.len()-1]))))
+        }
+
+        #[test]
+        fn obj_info_stmt_matches_keyword_freetext_and_a_newline_returns_everything(ref input in bytes_regex(r"obj_info[\x09\x20][\w]+\x0a").unwrap()) {
+            prop_assert_eq!(dbg_dmp(obj_info_stmt, "proptest_obj_info")(&input[..]), Ok((EMPTY, (OBJ_INFO, &input[9..input.len()-1]))))
         }
     }
 
@@ -134,7 +133,7 @@ mod tests {
 
         let r = ply_kwd(input);
 
-        assert_eq!(r, Ok((rest, PLY)));
+        assert_eq!(r, Ok((rest, PLY)))
     }
 
     #[test]
@@ -144,12 +143,6 @@ mod tests {
 
         let r = end_header_kwd(input.as_slice());
 
-        assert_eq!(r, Ok((rest.as_slice(), END_HEADER)));
-    }
-
-    #[test]
-    fn format_stmt_matches_keyword_format_and_version_and_returns_the_latter_two() {
-        let input = b"end_header\n1234\n";
-        let rest = b"\n1234\n";
+        assert_eq!(r, Ok((rest.as_slice(), END_HEADER)))
     }
 }
