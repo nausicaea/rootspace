9447fe7031a98efdeae6485e950a88332c70a2d2
Eleanor Young <developer@nausicaea.net>
2022-12-16 22:04:18 +0100

    Added more stuff

--- engine2/src/bin/wgpu.rs
+++ engine2/src/bin/wgpu.rs
@@ -3,45 +3,63 @@ use ecs::{ResourceRegistry, SystemRegistry, Reg};
 use winit::{event_loop::{EventLoop, EventLoopWindowTarget, ControlFlow}, window::{WindowBuilder, Window}, event::{Event, WindowEvent, KeyboardInput, ElementState, VirtualKeyCode, StartCause}};
 
 fn main() {
-    use pollster::FutureExt;
-
     env_logger::init();
 
-    let state: State<Reg![], Reg![], Reg![], Reg![]> = State::new("wgpu", false).block_on().unwrap();
 
     let event_loop = EventLoop::new();
     let window = WindowBuilder::new().build(&event_loop).unwrap();
 
-    event_loop.run(state.event_handler_factory(window))
+    let state: State<Reg![], Reg![], Reg![], Reg![]> = State::new(window, "wgpu", false).unwrap();
+
+    event_loop.run(state.event_handler_factory())
 }
 
 type World<S, F, D, R> = ecs::World<S, F, D, R>;
 
 struct State<S, F, D, R> {
     world: World<S, F, D, R>,
+    window: Window,
 }
 
 impl<S, F, D, R> State<S, F, D, R> 
 where
-    S: ResourceRegistry,
-    F: SystemRegistry,
-    D: SystemRegistry,
-    R: SystemRegistry,
+    S: 'static + ResourceRegistry,
+    F: 'static + SystemRegistry,
+    D: 'static + SystemRegistry,
+    R: 'static + SystemRegistry,
 {
-    async fn new<N: AsRef<str>>(name: N, force_init: bool) -> anyhow::Result<Self> {
+    fn new<N: AsRef<str>>(window: Window, name: N, force_init: bool) -> anyhow::Result<Self> {
         use try_default::TryDefault;
 
         let world = World::try_default()?;
 
-        Ok(State { world })
+        Ok(State { world, window })
     }
 
-    fn event_handler_factory(self, window: Window) -> impl 'static + FnMut(Event<'_, ()>, &EventLoopWindowTarget<()>, &mut ControlFlow) {
+    async fn init(&mut self) {
+        let size = self.window.inner_size();
+
+        // The instance is a handle to our GPU
+        // Backends::all => Vulkan + Metal + DX12 + Browser WebGPU
+        let instance = wgpu::Instance::new(wgpu::Backends::all());
+        let surface = unsafe { instance.create_surface(&self.window) };
+        let adapter = instance.request_adapter(
+            &wgpu::RequestAdapterOptions {
+                power_preference: wgpu::PowerPreference::default(),
+                compatible_surface: Some(&surface),
+                force_fallback_adapter: false,
+            },
+        ).await.unwrap();
+    }
+
+    fn event_handler_factory(mut self) -> impl 'static + FnMut(Event<'_, ()>, &EventLoopWindowTarget<()>, &mut ControlFlow) {
         move |event, _event_loop, control_flow| {
+            use pollster::FutureExt;
+
             trace!("{:?}", &event);
             match event { 
-                Event::NewEvents(StartCause::Init) => (),
-                Event::WindowEvent { window_id: w, event: ref e } if w == window.id() => {
+                Event::NewEvents(StartCause::Init) => self.init().block_on(),
+                Event::WindowEvent { window_id, event: ref e } if window_id == self.window.id() => {
                     match e {
                         WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
                         WindowEvent::KeyboardInput { input: KeyboardInput { state: ElementState::Pressed, virtual_keycode: Some(VirtualKeyCode::Escape), .. }, .. } => {
