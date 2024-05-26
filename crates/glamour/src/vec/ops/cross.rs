f6e641262deee30b599f7bd6223b5dff9f1817ba
Eleanor Young <developer@nausicaea.net>
2024-05-26 21:05:39 +0200

    Ok, the cross product does what I wanted it to

--- crates/glamour/src/vec/ops/cross.rs
+++ crates/glamour/src/vec/ops/cross.rs
@@ -19,3 +19,41 @@ where
 }
 
 forward_ref::forward_ref_binop!(impl<R: Float> Cross, cross for Vec4<R>, Vec4<R>, Vec4<R>);
+
+#[cfg(test)]
+mod tests {
+    use approx::ulps_eq;
+    use proptest::{prop_assert, proptest};
+
+    use crate::{ops::cross::Cross, test_helpers::proptest::{bounded_f32, vec4}};
+
+    proptest! {
+        #[test]
+        fn vec4_cross_is_equal_to_nalgebra(lhs in vec4(bounded_f32(-32, 32)), rhs in vec4(bounded_f32(-32, 32))) {
+            let glamour_result = lhs.cross(rhs);
+            let nalgebra_result = nalgebra::Vector3::new(lhs.x, lhs.y, lhs.z).cross(&nalgebra::Vector3::new(rhs.x, rhs.y, rhs.z));
+            let nalgebra_result = nalgebra::Vector4::new(nalgebra_result.x, nalgebra_result.y, nalgebra_result.z, 0.0);
+
+            prop_assert!(
+                ulps_eq!(glamour_result, nalgebra_result),
+                "\nglamour = {}\nnalgebra = {}",
+                glamour_result,
+                nalgebra_result,
+            );
+        }
+
+        #[test]
+        fn vec4_cross_is_equal_to_cgmath(lhs in vec4(bounded_f32(-32, 32)), rhs in vec4(bounded_f32(-32, 32))) {
+            let glamour_result = lhs.cross(rhs);
+            let cgmath_result = cgmath::Vector3::new(lhs.x, lhs.y, lhs.z).cross(cgmath::Vector3::new(rhs.x, rhs.y, rhs.z));
+            let cgmath_result = cgmath::Vector4::new(cgmath_result.x, cgmath_result.y, cgmath_result.z, 0.0);
+
+            prop_assert!(
+                ulps_eq!(glamour_result, cgmath_result),
+                "\nglamour = {:?}\ncgmath = {:?}",
+                glamour_result,
+                cgmath_result,
+            );
+        }
+    }
+}
