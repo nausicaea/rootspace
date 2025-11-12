8289ec4cc610ce76f425a79ad29b5926e80e7e35
Eleanor Young <developer@nausicaea.net>
2025-11-12 12:56:35 +0100

    chore: refine the nix-shell file, and add an additional pre-push handler

--- shell.nix
+++ shell.nix
@@ -1,7 +1,16 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import (fetchTarball https://github.com/NixOS/nixpkgs/archive/1af9d0d88739a3e0b0e64add12adce320ba1163f.tar.gz) {} }:
 
 pkgs.mkShell {
-  packages = [
+  buildInputs = [
+    pkgs.cacert
+    pkgs.git
+    pkgs.pre-commit
     pkgs.cargo
+    pkgs.cargo-nextest
+    pkgs.cargo-fuzz
+    pkgs.cargo-audit
+    pkgs.cargo-auditable
+    pkgs.tokio-console
   ];
+  RUST_LOG = "warn,rootspace=trace,griffon=trace";
 }
