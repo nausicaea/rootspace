5398b7d0385fb78790dcf697038c22d46f34d705
Eleanor Young <developer@nausicaea.net>
2026-01-01 17:17:56 +0100

    bugfix(wip, dorothy): bring the decoder implementation closer to the py-kcs reference

--- crates/dorothy/src/lib.rs
+++ crates/dorothy/src/lib.rs
@@ -4,15 +4,10 @@ pub fn decode<I: IntoIterator<Item = T>, T: Borrow<i16>>(channels: usize, sample
     use itertools::Itertools;
 
     // Fixed decoder parameters
-    //const BLOCK_SIZE: usize = 8196;
     const BASE_FREQ: u32 = 2400;
     let frames_per_bit: usize = (sample_rate as f64 * 8.0 / BASE_FREQ as f64).round() as usize;
 
     // Decoder state
-    //let mut per_channel_sample_buffer: Vec<[i16; BLOCK_SIZE]> = vec![[0; BLOCK_SIZE]; channels];
-    let mut fsms: Vec<BitDecoderFsm> = vec![BitDecoderFsm::default(); channels];
-    let mut look_behinds: Vec<RingBuffer<u8>> = vec![RingBuffer::new(frames_per_bit); channels];
-    let mut previous_sign_bits: Vec<u8> = vec![0; channels];
     let mut outputs: Vec<Vec<u8>> = vec![Vec::default(); channels];
 
     let framed_iter = samples.into_iter()
@@ -33,13 +28,9 @@ pub fn decode<I: IntoIterator<Item = T>, T: Borrow<i16>>(channels: usize, sample
         .chunk_by(|(_, channel_idx, _)| *channel_idx);
 
     for (channel_idx, channel_samples) in &per_channel_iter {
-        decode_channel(
+        outputs[channel_idx] = decode_channel(
             channel_samples.map(|(_, _, sample)| sample), 
             frames_per_bit,
-            &mut fsms[channel_idx],
-            &mut look_behinds[channel_idx],
-            &mut previous_sign_bits[channel_idx],
-            &mut outputs[channel_idx],
         );
     }
 
@@ -59,118 +50,67 @@ const fn to_sign_change(i: u8, previous: &mut u8) -> u8 {
     o
 }
 
+fn advance_n<I: Iterator<Item = u8>>(buf: &mut RingBuffer<u8>, i: &mut I, n: usize) {
+    i
+        .take(n)
+        .for_each(|item| buf.push(item));
+}
+
+fn sign_changes<T: Borrow<u8>, I: IntoIterator<Item = T>>(i: I) -> usize {
+    i.into_iter().map(|k| *k.borrow() as usize).sum()
+}
+
+const BITMASKS: [u8; 8] = [0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80];
+
 fn decode_channel(
     channel_data: impl Iterator<Item = i16>, 
     frames_per_bit: usize,
-    fsm: &mut BitDecoderFsm, 
-    look_behind: &mut RingBuffer<u8>, 
-    previous_sign_bit: &mut u8,
-    output: &mut Vec<u8>,
-) {
+) -> Vec<u8> {
+    // Per-channel decoder state
+    let mut look_behind: RingBuffer<u8> = RingBuffer::new(frames_per_bit);
+    let mut previous_sign_bit = 0_u8;
+    let mut output = Vec::default();
+
     let mut sign_change_iter = channel_data
-        .inspect(|sample| eprint!("{sample:x}"))
-        .map(|sample| to_sign_change(to_sign_bit(sample), previous_sign_bit));
+        .inspect(|sample| eprint!("{sample:x} -> "))
+        .map(|sample| to_sign_bit(sample))
+        .inspect(|sample| eprint!("{sample:x} -> "))
+        .map(|sample| to_sign_change(sample, &mut previous_sign_bit))
+        .inspect(|sample| eprintln!("{sample:x}"));
+
+    advance_n(&mut look_behind, sign_change_iter.by_ref(), frames_per_bit - 1);
+    let mut num_sign_changes = sign_changes(&look_behind.0);
 
     while let Some(sign_change) = sign_change_iter.next() {
+        if sign_change != 0 {
+            num_sign_changes += 1;
+        }
+        if look_behind.pop() != Some(0) {
+            num_sign_changes -= 1;
+        }
         look_behind.push(sign_change);
-        let num_sign_changes: usize = look_behind.0.iter()
-            .map(|&k| k as usize)
-            .sum();
 
-        if let Some(decoded_byte) = fsm.next(frames_per_bit, num_sign_changes, &mut sign_change_iter) {
-            output.push(decoded_byte);
+        // If a start bit is detected, sample the next 8 data bits
+        if num_sign_changes <= 9 {
+            let mut byteval = 0_u8;
+            for mask in BITMASKS {
+                advance_n(&mut look_behind, sign_change_iter.by_ref(), frames_per_bit);
+                if sign_changes(&look_behind.0) >= 12 {
+                    byteval |= mask;
+                }
+            }
+            output.push(byteval);
+
+            // Skip the final two stop bits and refill the sample buffer
+            sign_change_iter.by_ref()
+                .skip(2 * frames_per_bit)
+                .take(3 * frames_per_bit - 1)
+                .for_each(|item| look_behind.push(item));
+            num_sign_changes = sign_changes(&look_behind.0);
         }
     }
-}
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-enum BitDecoderFsm {
-    DetectStartBit,
-    DecodeDataBits {
-        bitmask_idx: usize,
-        current_byte: u8,
-    },
-    DetectStopBit {
-        bit_idx: usize,
-        decoded_byte: u8,
-    },
-}
-
-impl BitDecoderFsm {
-    const BITMASKS: [u8; 8] = [0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80];
-    const LOW_THRESHOLD: usize = 9;
-    const HIGH_THRESHOLD: usize = 12;
-
-    fn next(&mut self, frames_per_bit: usize, num_sign_changes: usize, data: &mut impl Iterator<Item = u8>) -> Option<u8> {
-        let mut output = None;
-
-        use BitDecoderFsm::*;
-        *self = match *self {
-            // Transition to decoding data if the number of sign changes is low, i.e.
-            // the audio signal has a low frequency
-            DetectStartBit if num_sign_changes <= Self::LOW_THRESHOLD => {
-                DecodeDataBits { 
-                    bitmask_idx: 0,
-                    current_byte: 0x00,
-                }
-            },
-            // Continue searching for the start bit
-            DetectStartBit => DetectStartBit,
-            DecodeDataBits { bitmask_idx, current_byte } => {
-                if bitmask_idx < Self::BITMASKS.len() - 1 {
-                    let mut lookahead = Vec::default();
-                    for _ in 0..frames_per_bit {
-                        let Some(sign_change) = data.next() else {
-                            break
-                        };
-                        lookahead.push(sign_change);
-                    }
-                    let lookahead_num_sign_changes: usize = lookahead.iter()
-                        .map(|&k| k as usize)
-                        .sum();
-
-                    DecodeDataBits { 
-                        bitmask_idx: bitmask_idx + 1,
-                        current_byte: if lookahead_num_sign_changes >= Self::HIGH_THRESHOLD {
-                            current_byte | Self::BITMASKS[bitmask_idx]
-                        } else {
-                            current_byte
-                        },
-                    }
-                } else {
-                    DetectStopBit {
-                        bit_idx: 0, 
-                        decoded_byte: current_byte,
-                    }
-                }
-            },
-            DetectStopBit { bit_idx: 0, decoded_byte } => {
-                // if num_sign_changes < Self::HIGH_THRESHOLD {
-                //     eprintln!("Expected the first stop bit (high frequency)");
-                // }
-                DetectStopBit {
-                    bit_idx: 1,
-                    decoded_byte,
-                }
-            },
-            DetectStopBit { bit_idx: 1, decoded_byte } => {
-                // if num_sign_changes < Self::HIGH_THRESHOLD {
-                //     eprintln!("Expected the second stop bit (high frequency)");
-                // }
-                output = Some(decoded_byte);
-                DetectStartBit
-            },
-            state => panic!("Unknown state: {state:?}"),
-        };
-
-        output
-    }
-}
-
-impl Default for BitDecoderFsm {
-    fn default() -> Self {
-        BitDecoderFsm::DetectStartBit
-    }
+    output
 }
 
 #[derive(Debug, Clone, Default)]
@@ -292,11 +232,11 @@ mod tests {
 
     #[test]
     fn decode_accepts_i16_iterator_input() {
-        decode(2, 2, [0_i16; 4]);
-        decode(2, 2, &[0_i16; 4]);
-        decode(2, 2, vec![0_i16; 4]);
-        decode(2, 2, &vec![0_i16; 4]);
-        decode(2, 2, std::iter::once(0_i16));
+        decode(1, 4, [0_i16; 4]);
+        decode(1, 4, &[0_i16; 4]);
+        decode(1, 4, vec![0_i16; 4]);
+        decode(1, 4, &vec![0_i16; 4]);
+        decode(1, 4, std::iter::once(0_i16));
     }
 
     #[rstest]
@@ -308,6 +248,11 @@ mod tests {
     }
 
     proptest! {
+        #[test]
+        fn to_sign_bit_is_one_for_negative_input(input: i16) {
+            prop_assert_eq!(to_sign_bit(input), if input < 0 { 1_u8 } else { 0 });
+        }
+
         #[test]
         fn to_sign_change_xors_input_and_previous(input: u8, previous: u8) {
             let mut p = previous;
@@ -337,9 +282,11 @@ mod tests {
         let mut expected_data = Vec::new();
         BufReader::new(File::open(TEST_DIR.join(expected)).unwrap()).read_to_end(&mut expected_data).unwrap();
 
-        //assert_eq!(output.len(), expected_data.len());
-        eprintln!("{output:?}");
-        assert_eq!(output[0], expected_data);
+        assert_eq!(output.len(), 1);
+        let output = &output[0];
+
+        assert_eq!(output.len(), expected_data.len());
+        assert_eq!(output, &expected_data);
     }
 
 
