3d27425a31abf2d0f15024e41176697404631dae
Eleanor Young <developer@nausicaea.net>
2026-01-17 08:12:12 +0100

    feature(dorothy): working on prototyping the goertzel algorithm

---
+++ contrib/goertzel/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Python development environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            (pkgs.python3.withPackages (python-pkgs: [
+                python-pkgs.jupyter
+                python-pkgs.numpy
+                python-pkgs.matplotlib
+            ]))
+          ];
+        };
+      }
+    );
+}
+
